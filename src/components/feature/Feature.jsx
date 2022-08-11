844cd1a799c0d219910096940d53ed20f69e8a10
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-08-11 23:54:53 +0500

    Added Feature content

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './feature.css';
 
-function Feature(props) {
+function Feature({title, text}) {
     return (
-        <div>
-            Feature
+        <div className="gpt3__feature-container__feature">
+            <div className="gpt3__feature-container__feature-title">
+                <div/>
+                <h1>{title}</h1>
+            </div>
+            <div className="gpt3__feature-container_feature-text">
+                {text}
+            </div>
         </div>
     );
 }
