e6e0fcc9b5c2c65e20f5a14107252a7d14c3bf1b
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-08-22 22:58:07 +0500

    Addd basic styling to features section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -24,7 +24,7 @@ const featuresData = [
 
 function Features(props) {
     return (
-        <div className="gpt3__fetures section__padding">
+        <div className="gpt3__features section__padding">
             <div className="gpt3__features-heading">
                 <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today
                     & Make it Happen.</h1>
