e1a1a6cadcc9fcbee6760e31c55bd7344aacaaa7
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-08-31 01:03:56 +0500

    Added Article content

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './article.css';
-function Article(props) {
+
+function Article({imgUrl, date, title}) {
     return (
-        <div>
-            Article
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-image">
+                <img src={imgUrl} alt="blog"/>
+            </div>
+            <div className="gpt3__blog-container_article-content">
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
         </div>
     );
 }
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import blog01 from '../../assets/blog01.png'
+import blog02 from '../../assets/blog02.png'
+import blog03 from '../../assets/blog03.png'
+import blog04 from '../../assets/blog04.png'
+import blog05 from '../../assets/blog05.png'
+
+export {
+    blog04,
+    blog05,
+    blog03,
+    blog01,
+    blog02
+}
